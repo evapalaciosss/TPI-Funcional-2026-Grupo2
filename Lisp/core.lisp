523e14172742f05c19217ecb9d897acc50a22452
jeres-ic <ivan.caraballoj@gmail.com>
2026-06-11 20:39:09 -0300

    Agregue el requerimiento 2 comentado

--- Lisp/core.lisp
+++ Lisp/core.lisp
@@ -21,3 +21,22 @@
         )
     )
 )
+
+;; ======================================================== 
+;; FUNCION: timer
+;; NATURALEZA:  Pura (no devuelve nada en pantalla)
+;; ESTRATEGIA: Seleccion condicional
+;; IMPACTO: No destructiva
+;; ======================================================== 
+(defun timer (unix-time)    ;;unix-time es el tiempo epoch
+    (cond
+        ((<= (mod unix-time 216) 89) 'rojo)
+        ((>= (mod unix-time 216) 210) 'amarillo)    ;;creo que es la manera maeficiente
+        (t                                          
+            'verde
+        )
+    )
+)
+;;del 0 al 89 segundos va a ser rojo, de 90 al 209 va a ser verde y del 210 al 215 va a ser amarillo
+;;la intencion es dejar en caso de que no sea ni menor o igual a 89, ni mayor o igual a 210
+;;asi simplemente queda ese intervalo de segundos en medio para el verde, quedando en el ultimo cond
