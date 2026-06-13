c221e5a6a386c9c7d806efec1d07b7340238107d
gonzalo-uc <gonzalourielcastillo@gmail.com>
2026-06-13 01:15:25 -0300

    Agrego requerimiento n°6

--- Lisp/core.lisp
+++ Lisp/core.lisp
@@ -40,3 +40,70 @@
 ;;del 0 al 89 segundos va a ser rojo, de 90 al 209 va a ser verde y del 210 al 215 va a ser amarillo
 ;;la intencion es dejar en caso de que no sea ni menor o igual a 89, ni mayor o igual a 210
 ;;asi simplemente queda ese intervalo de segundos en medio para el verde, quedando en el ultimo cond
+
+
+;;Requerimiento N°3
+
+;; =================================================================================================================
+;; FUNCIÓN: crear-ciclos
+;; NATURALEZA: Pura (Retorna la cantidad de ciclos completos)
+;; ESTRATEGIA: Transformacion aritmetica (Convierte minutos a segundos y calcula ciclos completos mediante floor)
+;; IMPACTO: No destructiva (No modifica variables ni estructuras)
+;; PROPOSITO: Determinar cuantos ciclos completos de 216 segundos pueden realizarse en un intervalo de tiempo dado.
+;; =================================================================================================================
+
+(defun crear-ciclos (minutos)
+  (floor (/ (* minutos 60) 216)))
+
+;; ===================================================================================================================================
+;; FUNCIÓN: informe-distribucion
+;; NATURALEZA: Impura (Además de calcular porcentajes, muestra resultados por pantalla mediante format)
+;; ESTRATEGIA: Calculo directo (Utiliza funciones y operaciones aritmeticas para distribuir el tiempo entre los estados del semaforo)
+;; IMPACTO: No destructiva (No modifica variables externas ni estructuras de datos)
+;; PROPOSITO: Calcular y mostrar el porcentaje de tiempo que el semaforo permanece en rojo, verde y amarillo
+;; ===================================================================================================================================
+
+(defun informe-distribucion (minutos)
+  (let* (
+         ;; convertimos los minutos a segundos
+         (total-segundos (* minutos 60.0))
+
+         ;; cantidad de ciclos completos
+         (ciclos (crear-ciclos minutos))
+
+         ;; guarda los segundos que sobran despues de los ciclos completos con mod
+         (segundos-sobra (mod (* minutos 60) 216))
+
+         ;; el rojo usa 90 segundos del sobrante
+         ;; min agarra el valor mas chico, para qu si sobra menos de 90 lo agarre todo, y si sobra mas, que solo agarre 90
+         (sobra-rojo (min segundos-sobra 90))
+
+         ;; restamos el tiempo usado por el rojo para darle sobrante al verde
+         ;; max agarra el valor mas grande para asegurarnos de que no agarre un numero en negativo en caso de que ya no sobre nada
+         (sobra-verde (min (max 0 (- segundos-sobra 90)) 120))
+
+         ;; al sobrante le restamos el tiempo que usaron el rojo y el verde
+         (sobra-amarillo (max 0 (- segundos-sobra 210)))
+
+         ;; calculamos los segundos totales de cada color
+         (segundos-rojo (+ (* ciclos 90) sobra-rojo))
+         (segundos-verde (+ (* ciclos 120) sobra-verde))
+         (segundos-amarillo (+ (* ciclos 6) sobra-amarillo))
+
+         ;; calculamos los porcentajes
+         (porcentaje-rojo (* (/ segundos-rojo total-segundos) 100))
+         (porcentaje-verde (* (/ segundos-verde total-segundos) 100))
+         (porcentaje-amarillo (* (/ segundos-amarillo total-segundos) 100)))
+
+    ;; mostramos
+    (format t "~%Informe de Distribución Temporal (~A minutos):~%~%" minutos)
+    (format t "Porcentaje Rojo: ~,2F%~%" porcentaje-rojo)
+    (format t "Porcentaje Verde: ~,2F%~%" porcentaje-verde)
+    (format t "Porcentaje Amarillo: ~,2F%~%" porcentaje-amarillo)))
+
+;; Casos de prueba:                    Resultado esperado:
+;; (informe-distribucion 1)            Rojo=100.00%, Verde=0.00%, Amarillo=0.00%
+;; (informe-distribucion 2)            Rojo=75.00%, Verde=25.00%, Amarillo=0.00%
+;; (informe-distribucion 15)           Rojo=44.00%, Verde=53.33%, Amarillo=2.67%
+;; (informe-distribucion 30)           Rojo=44.00%, Verde=53.33%, Amarillo=2.67%
+;; (informe-distribucion 60)           Rojo=42.50%, Verde=54.83%, Amarillo=2.67%
