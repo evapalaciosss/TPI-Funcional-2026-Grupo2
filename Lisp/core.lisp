116fa7d726a1190f6a76e054742a6c5b04df57bc
jeres-ic <ivan.caraballoj@gmail.com>
2026-06-15 12:43:35 -0300

    arregle funcion auditoria-aux

--- Lisp/core.lisp
+++ Lisp/core.lisp
@@ -70,16 +70,16 @@
     (cond 
         ((> tiempo limite) NIL)
         (t
-            (let ((resto (mod unix-time 216)))
+            (let ((resto (mod tiempo 216)))
                 (cond
                     ((= resto 0)                  ;;si coindice con el tiempo pasado significa que hubo un cambio de una transicion 
-                        (format t "Tiempo ~A: La luz a cambiado de amarillo a rojo" unix-time)   
+                        (format t "Tiempo ~A: La luz a cambiado de amarillo a rojo" tiempo)   
                     )
                     ((= resto 90)
-                        (format t "Tiempo ~A: La luz a cambiado de rojo a verde" unix-time)
+                        (format t "Tiempo ~A: La luz a cambiado de rojo a verde" tiempo)
                     )
                     ((= resto 210) 
-                        (format t "Tiempo ~A: La luz a cambiado de verde a amarillo" unix-time)
+                        (format t "Tiempo ~A: La luz a cambiado de verde a amarillo" tiempo)
                     )
                 )
             )
