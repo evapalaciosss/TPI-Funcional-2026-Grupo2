a690ab514757314319c4e88ec2d63484f1ad4351
jeres-ic <ivan.caraballoj@gmail.com>
2026-06-15 22:29:09 -0300

    borre comentarios

--- Lisp/core.lisp
+++ Lisp/core.lisp
@@ -280,36 +280,7 @@
 ;; ESTRATEGIA: Recursividad simple
 ;; IMPACTO: No destructiva
 ;; ======================================================== 
-(defun auditoria-aux (tiempo limite)    ;;hace la recursividad hasta el limite de 10 min
-    (cond 
-        ((> tiempo limite) NIL)
-        (t
-           (let ((resto (mod tiempo 225)))
-                (cond
-                    ((= resto 0)                  ;;si coindice con el tiempo pasado significa que hubo un cambio de una transicion 
-                       (format t "Tiempo ~A: La luz a cambiado de amarillo-intermitente a rojo~%" tiempo)   
-                    )
-                    ((= resto 90)
-                       (format t "Tiempo ~A: La luz a cambiado de rojo a amarillo-intermitente~%" tiempo)
-                    )
-                    ((= resto 93)
-                       (format t "Tiempo ~A: La luz a cambiado de amarillo-intermitente a verde~%" tiempo)
-                   )
-                    ((= resto 213) 
-                        (format t "Tiempo ~A: La luz a cambiado de verde a amarillo-intermitente~%" tiempo)
-                   )
-                    ((= resto 216) 
-                        (format t "Tiempo ~A: La luz a cambiado de amarillo-intermitente a amarillo~%" tiempo)
-                   )
-                    ((= resto 222) 
-                        (format t "Tiempo ~A: La luz a cambiado de amarillo a amarillo-intermitente ~%" tiempo)
-                   )
-                )
-            )
-            (auditoria-aux (+ tiempo 1) limite)     ;;va sumando de a 1 segundo
-       )
-    )
-)
+
 ;; ========================================================
 ;; FUNCIÓN: duracion-ciclo
 ;; NATURALEZA: Pura
