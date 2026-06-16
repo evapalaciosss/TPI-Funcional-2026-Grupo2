42aaa3dc907358566f918c57a87d18f939140773
jeres-ic <ivan.caraballoj@gmail.com>
2026-06-15 22:21:42 -0300

    actualizacion de todo el codigo clojure

--- comparativa/Requerimientos 1 y 2 - Clojure.clj
+++ comparativa/Requerimientos 1 y 2 - Clojure.clj
@@ -12,21 +12,21 @@
 (defn transicion [color-actual cambiar-a] 
     (cond
         ((and (= color-actual 'en-rojo) (= cambiar-a 'verde-intermitente)) 
-             (list color-actual 'Cambiar-a-verde-intermiente)      
+             (list color-actual 'Cambiar-a-verde-intermitente)      
         )
-        ((and (= color-actual 'en-verde-intermiente) (= cambiar-a 'verde))
+        ((and (= color-actual 'en-verde-intermitente) (= cambiar-a 'verde))
             (list color-actual 'Cambiar-a-verde)
         )
-        ((and (= color-actual 'en-verde) (= cambiar-a 'amarillo-intermiente))
-            (list color-actual 'Cambiar-a-amarillo-intermiente)
+        ((and (= color-actual 'en-verde) (= cambiar-a 'amarillo-intermitente))
+            (list color-actual 'Cambiar-a-amarillo-intermitente)
         )
         ((and (= color-actual 'en-amarillo-intermitente) (= cambiar-a 'amarillo)) 
              (list color-actual 'Cambiar-a-amarillo)      
         )
         ((and (= color-actual 'en-amarillo) (= cambiar-a 'rojo-intermitente)) 
-             (list color-actual 'Cambiar-a-rojo-intermiente)      
+             (list color-actual 'Cambiar-a-rojo-intermitente)      
         )
-        ((and (= color-actual 'en-rojo-intermiente) (= cambiar-a 'rojo)) 
+        ((and (= color-actual 'en-rojo-intermitente) (= cambiar-a 'rojo)) 
              (list color-actual 'Cambiar-a-rojo)      
         )
         :else      
@@ -68,28 +68,33 @@
 ;;   en más de un lugar del cond. Sin let, tendríamos que
 ;;   recalcular mod tres veces, lo cual es innecesario.
 ;; ======================================================================
-
-(defn timer [unix-time duracion-rojo duracion-amarillo duracion-verde]
-  (let [duracion-ciclo (+ duracion-rojo duracion-amarillo duracion-verde)
+(defn timer [unix-time]
+  (let [duracion-ciclo 225
         posicion       (mod unix-time duracion-ciclo)]
     (cond
-      (< posicion duracion-rojo)
+      (< posicion 90)
       'rojo
 
-      (< posicion (+ duracion-rojo duracion-amarillo))
+      (< posicion 93)
+      'verde-intermitente
+
+      (< posicion 213)
+      'verde
+
+      (< posicion 216)
+      'amarillo-intermitente
+
+      (< posicion 222)
       'amarillo
 
       :else
-      'verde)))
+      'rojo-intermitente)))
     
 ;; Casos Prueba                      Salida
-;; (timer 0   90 6 120)              rojo
-;; (timer 90  90 6 120)              amarillo
-;; (timer 96  90 6 120)              verde
-;; (timer 216 90 6 120)              rojo    (nuevo ciclo)
-
-;; Camino Alternativo                Salida
-;; (timer 40  30 5 60)               amarillo
+;; (timer 0 )              rojo
+;; (timer 90)              verde-intermitente
+;; (timer 96)              verde
+;; (timer 221)             amarillo   
 
 ;; Caso Error (tiempo invalido)      Salida
-;; (timer -10  90 6 120)             verde   (mod negativo, resultado invalido)
+;; (timer -10)             verde   (mod negativo, resultado invalido)
